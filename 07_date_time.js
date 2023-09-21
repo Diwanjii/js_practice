9c147e0a73e26ecff390fbe2c28da32c28a4b017
Diwanjii <145532084+Diwanjii@users.noreply.github.com>
2023-09-21 10:07:26 +0000

    Date And Time

---
+++ 07_date_time.js
@@ -0,0 +1,30 @@
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.getDate());
+// console.log(myDate.getFullYear());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0 , 23)
+// let myCreatedDate = new Date(2023, 0 , 23 , 5 , 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+
+// console.log(Math.floor(myDate/1000));
+
+
+
+
+
+
+
