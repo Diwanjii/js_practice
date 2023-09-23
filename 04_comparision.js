6659dcdcb13e1be63bbc5fc2d4e76f62c9506056
Diwanjii <145532084+Diwanjii@users.noreply.github.com>
2023-09-23 08:59:15 +0000

    heap_stack

--- 04_comparision.js
+++ 04_comparision.js
@@ -2,3 +2,25 @@ console.log("2" < 1); //false
 console.log(undefined == 0);
 console.log(undefined < 0);
 console.log(undefined > 0);
+
+// Stack and Heap
+
+// Primitive ---> Stack
+// copy data
+let myNameIs = "Anish Kumar";
+let yourNameIs = myNameIs; // copy data
+yourNameIs = "javascript"; // this will not effect myNameIs variable
+
+// console.log(myNameIs);
+// console.log(yourNameIs);
+
+let userOne = {
+    email: "anish@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne; // it will get refrence .
+userTwo.upi = "user@ibl"; // it will change main variable
+
+console.log(userOne);
+console.log(userTwo);
