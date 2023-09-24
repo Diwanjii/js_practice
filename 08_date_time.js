0d1a8ee4ec8e774e744245e50959517309eba4fb
Diwanjii <145532084+Diwanjii@users.noreply.github.com>
2023-09-24 17:56:03 +0000

    date_time

--- 08_date_time.js
+++ 08_date_time.js
@@ -1,21 +1,50 @@
-
+// The Date object works with dates and times.
+// Date objects are created with new Date().
+// There are four ways of instantiating (creating) a date:
 let myDate = new Date();
-// console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.getDate());
-// console.log(myDate.getFullYear());
-// console.log(myDate.toJSON());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
-
 // let myCreatedDate = new Date(2023, 0 , 23)
 // let myCreatedDate = new Date(2023, 0 , 23 , 5 , 3)
 // let myCreatedDate = new Date("2023-01-14")
 // let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
+// console.log(myDate);
 
+// -------------------------- toString() ----------------------------
+// The toString() method returns a date object as a string.
+console.log(myDate.toString());
+
+// ------------------------- toDateString() ----------------------------
+// The toDateString() method returns the date (not the time) of a date object as a string.
+console.log(myDate.toDateString());
+
+// ----------------------- getDate() ---------------------------
+// The getDate() method returns the day of the month (1 to 31) of a date.
+console.log(myDate.getDate());
+
+// --------------------- getFullYear() ----------------------
+// getFullYear() returns the full year (4 digits) of a date.
+console.log(myDate.getFullYear());
+
+// ------------------------ toJSON() ------------------------
+// The toJSON() method returns a date object as a string, formatted as a JSON date.
+// JSON dates have the same format as the ISO-8601 standard: YYYY-MM-DDTHH:mm:ss.sssZ
+console.log(myDate.toJSON());
+
+// --------------------- toISOString() -----------------------
+// The toISOString() method returns a date object as a string, using the ISO standard.
+// The standard is called ISO-8601 and the format is: YYYY-MM-DDTHH:mm:ss.sssZ
+console.log(myDate.toISOString());
+
+// -------------------- toLocaleString() ------------------------
+// The toLocaleString() method returns a Date object as a string, using locale settings.
+// The default language depends on the locale setup on your computer.
+console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// console.log(myCreatedDate.toLocaleString());
+
+// ------------------ Date.now() ------------------
+// Date.now() returns the number of milliseconds since January 1, 1970.
 let myTimeStamp = Date.now()
 // console.log(myTimeStamp);
 // console.log(myDate.getTime());
