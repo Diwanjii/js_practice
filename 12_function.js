545d3d86a66d49966496e56421ad55228e0c0842
Diwanjii <145532084+Diwanjii@users.noreply.github.com>
2023-09-27 19:20:53 +0000

    Function

---
+++ 12_function.js
@@ -0,0 +1,60 @@
+// =============================================== Function ====================================================
+// When developing an application, you often need to perform the same action in many places. 
+// For example, you may want to show a message whenever an error occurs.
+
+// To avoid repeating the same code all over places, you can use a function to wrap that code and reuse it.
+
+// JavaScript provides many built-in functions such as parseInt() and parseFloat(). In this tutorial, 
+// you will learn how to develop custom functions.
+
+// To declare a function, you use the function keyword, followed by the function name, a list of parameters, 
+// and the function body as follows:
+function sayMyName() {
+    console.log("Hello");
+    console.log("Anish Diwan");
+}
+// sayMyName // It is Refrence
+// sayMyName()
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(); // NaN
+addTwoNumbers(3, 4) // 7
+addTwoNumbers(3, "4") // 34
+addTwoNumbers(3, null) // 3
+
+const result = addTwoNumbers(3, 5);
+console.log(result); // undefined
+
+function addTwoNumber(number1, number2) {
+    return number1 + number2;
+}
+
+const results = addTwoNumber(3, 5);
+console.log("Results : ", results); // Results : 8
+
+// function loginUserMessage(username) {
+//     if (username === undefined) {
+//         console.log("Please enter a username");
+//         return
+//     } else {
+//         return `${username} just logged in.`;
+//     }
+// }
+
+// console.log(loginUserMessage("Anish")); // Anish just logged in.
+// console.log(loginUserMessage()); // Please enter a username
+
+function loginUserMessage(username = "Anish") {
+    if (username === undefined) {
+        console.log("Please enter a username");
+        return
+    } else {
+        return `${username} just logged in.`;
+    }
+}
+
+console.log(loginUserMessage()); // Anish just logged in.
+console.log(loginUserMessage("Sam")); // Sam just logged in.
